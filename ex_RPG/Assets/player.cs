d1485927314a63003ea2e2f7dff39ea84bd70eb6
segaWorld <biton1993@gmail.com>
2019-04-06 23:56:31 +0900

    add: first commit

---
+++ ex_RPG/Assets/player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public float speed = 30;
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+
+        float y = Input.GetAxisRaw("Vertical");
+
+        Vector2 direction = new Vector2(x, y).normalized;
+
+        GetComponent<Rigidbody2D>().velocity = direction * speed;
+    }
+}
