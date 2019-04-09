0908dac753f3b1674a2de05a2862299a6b4ed8a9
yonikkusu <yonikkusu@gmail.com>
2019-04-10 01:59:29 +0900

    add: シーンマネージャー

---
+++ Assets/MyAssets/Scripts/Commons/SceneManager.cs
@@ -0,0 +1,31 @@
+﻿using SceneManagement = UnityEngine.SceneManagement;
+
+//--------------------------------------------------------------------------/
+/// <summary>
+/// シーンマネージャー
+/// </summary>
+//--------------------------------------------------------------------------/
+public class SceneManager : SingletonMonoBehaviour<SceneManager>
+{
+    //--------------------------------------------------------------------------/
+    /// <summary>
+    /// シーンを読み込む
+    /// </summary>
+    /// <param name="scene">読み込むシーン名</param>
+    //--------------------------------------------------------------------------/
+    public void LoadScene(Scene scene)
+    {
+        SceneManagement.SceneManager.LoadSceneAsync(scene.ToString());
+    }
+}
+
+//--------------------------------------------------------------------------/
+/// <summary>
+/// シーン名
+/// </summary>
+//--------------------------------------------------------------------------/
+public enum Scene
+{
+	Title,
+	MainMenu,
+}
---
+++ Assets/MyAssets/Scripts/Commons/SingletonMonoBehaviour.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System;
+
+//--------------------------------------------------------------------------/
+/// <summary>
+/// シングルトン
+/// </summary>
+//--------------------------------------------------------------------------/
+public abstract class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+    /// <summary>自身のインスタンス</summary>
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null) {
+                Type t = typeof(T);
+
+                instance = (T)FindObjectOfType(t);
+                if (instance == null) {
+                    Debug.LogError(t + " をアタッチしているGameObjectはありません");
+                }
+            }
+
+            return instance;
+        }
+    }
+    private static T instance;
+
+    //--------------------------------------------------------------------------/
+    /// <summary>
+    /// 起動時処理
+    /// </summary>
+    //--------------------------------------------------------------------------/
+    virtual protected void Awake()
+    {
+        CheckInstance();
+    }
+
+    //--------------------------------------------------------------------------/
+    /// <summary>
+    /// 他のゲームオブジェクトにアタッチされているか調べる
+    /// アタッチされている場合は破棄する
+    /// </summary>
+    //--------------------------------------------------------------------------/
+    protected bool CheckInstance()
+    {
+        if(instance == null) {
+            instance = this as T;
+            return true;
+        }else if(Instance == this) {
+            return true;
+        }
+        Destroy(this);
+        return false;
+    }
+}
