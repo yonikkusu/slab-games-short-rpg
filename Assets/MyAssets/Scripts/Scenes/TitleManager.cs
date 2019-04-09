36a7bfc03cba10d734ba4460f845d128d6eca7e9
yonikkusu <yonikkusu@gmail.com>
2019-04-10 02:02:42 +0900

    add: タイトル画面→メインメニュー画面への遷移

---
+++ Assets/MyAssets/Scripts/Scenes/TitleManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+//--------------------------------------------------------------------------/
+/// <summary>
+/// タイトルマネージャー
+/// </summary>
+//--------------------------------------------------------------------------/
+public class TitleManager : MonoBehaviour
+{
+    [SerializeField] private Button startButton;
+
+    //--------------------------------------------------------------------------/
+    /// <summary>
+    /// 起動時処理
+    /// </summary>
+    //--------------------------------------------------------------------------/
+    void Start()
+    {
+        // スタートボタンタップでメインメニュー画面へ遷移
+        startButton.onClick.AddListener(() => SceneManager.Instance.LoadScene(Scene.MainMenu));
+    }
+}
