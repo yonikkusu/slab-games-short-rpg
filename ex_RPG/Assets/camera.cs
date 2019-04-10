c09276ed587bf655e145da54fddbe747803e5950
segaWorld <biton1993@gmail.com>
2019-04-11 00:12:21 +0900

    add: player animation, camera movement with player, switch scene

---
+++ ex_RPG/Assets/camera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+       // Debug.Log("x: " + player.transform.position.x + "y: " + player.transform.position.y);
+
+    }
+}
--- ex_RPG/Assets/player.cs
+++ ex_RPG/Assets/player.cs
@@ -1,25 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class player : MonoBehaviour
 {
+    public float speed = 30;
+    Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
-    public float speed = 30;
     // Update is called once per frame
     void Update()
     {
         float x = Input.GetAxisRaw("Horizontal");
-
         float y = Input.GetAxisRaw("Vertical");
 
         Vector2 direction = new Vector2(x, y).normalized;
-
         GetComponent<Rigidbody2D>().velocity = direction * speed;
+
+        // アニメーショントリガー
+        if (Input.GetKey(KeyCode.LeftArrow))
+        { 
+            anim.SetTrigger("left");
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            anim.SetTrigger("right");
+        }
+        else if(Input.GetKey(KeyCode.UpArrow))
+        {
+            anim.SetTrigger("up");
+        }
+        else if(Input.GetKey(KeyCode.DownArrow))
+        {
+            anim.SetTrigger("down");
+        }
+
     }
+
+    /*
+    void OnCollisionEnter( Collision collision )
+    {
+        SceneManager.LoadScene("battle_Scene");
+        Debug.Log("Load Scene");
+        Debug.Log(collision.gameObject.name);
+    }
+    */
+
+    
+    private void OnTriggerEnter2D( Collider2D other )
+    {
+        SceneManager.LoadScene("battle_Scene");
+    }
+    
 }
