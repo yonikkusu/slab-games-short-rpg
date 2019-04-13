afb302afa541f72194a4db0164739b9df589e8f6
hydeb104 <49332073+hydeb104@users.noreply.github.com>
2019-04-13 22:42:34 +0900

    upd8:エンドクレジット追加とBGMの追加、プロジェクトファイルの上げ直し

---
+++ Novel Test/Assets/GameSystem.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using Fungus;
+
+public class GameSystem : MonoBehaviour {
+
+    public void GameStart() {
+        SceneManager.LoadScene ("Game");
+    }
+
+    public void EndCredits()
+    {
+        SceneManager.LoadScene("EndCredits");
+    }
+
+    //　ゲーム終了ボタンを押したら実行する
+    public void GameEnd() {
+	#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+	#elif UNITY_WEBPLAYER
+		Application.OpenURL("http://www.yahoo.co.jp/");
+	#else
+		Application.Quit();
+	#endif
+    }
+
+
+}
+ 
