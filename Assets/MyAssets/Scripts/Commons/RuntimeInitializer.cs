c5b5f59707bfbb9a7facf7cb188414f57190fe4b
yonikkusu <yonikkusu@gmail.com>
2019-04-20 12:19:30 +0900

    add: DontDestroyObjectを生成するRuntimeInitializerを作成、シーンマネージャーに適用

---
+++ Assets/MyAssets/Scripts/Commons/RuntimeInitializer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+//--------------------------------------------------------------------------/
+/// <summary>
+/// ゲーム中に常に存在させるオブジェクトの初期化クラス
+/// </summary>
+//--------------------------------------------------------------------------/
+public class RuntimeInitializer
+{
+    //--------------------------------------------------------------------------/
+    /// <summary>
+    /// 各マネージャーをDontDestroyObjectとして生成する
+    /// </summary>
+    //--------------------------------------------------------------------------/
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    private static void InitializeBeforeSceneLoad()
+    {
+        // シーンマネージャー
+        var sceneManager = GameObject.Instantiate(Resources.Load("Prefabs/Commons/SceneManager"));
+        GameObject.DontDestroyOnLoad(sceneManager);
+    }
+}
