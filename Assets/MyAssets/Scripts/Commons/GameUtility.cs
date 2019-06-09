93b8f7b95ae05330353f74dfe2c16763d9f7bc79
yonikkusu <yonikkusu@gmail.com>
2019-06-09 15:36:52 +0900

    add: 現在UnixTime取得関数とUnixTimeをDateTimeOffsetに変換する関数を追加

---
+++ Assets/MyAssets/Scripts/Commons/GameUtility.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+//--------------------------------------------------------------------------/
+/// <summary>
+/// ゲームで使えそうなユーティリティ
+/// </summary>
+//--------------------------------------------------------------------------/
+public class GameUtility
+{
+    /// <summary>UnixTimeの起点時刻</summary>
+    private static readonly DateTimeOffset UnixEpoch = new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero);
+
+    //--------------------------------------------------------------------------/
+    /// <summary>
+    /// 現在のUnixTimeを取得する
+    /// </summary>
+    /// <returns>UnixTime</returns>
+    //--------------------------------------------------------------------------/
+    public static long GetUnixTime()
+    {
+        return (DateTimeOffset.Now - UnixEpoch).Ticks / 10000000;
+    }
+
+    //--------------------------------------------------------------------------/
+    /// <summary>
+    /// UnixTimeをDateTimeOffsetに変換する
+    /// </summary>
+    /// <param name="unixTime">変換したいUnixTime</param>
+    /// <returns>DateTimeOffset</returns>
+    //--------------------------------------------------------------------------/
+    public static DateTimeOffset UnixTimeToDateTimeOffset(long unixTime)
+    {
+        return new DateTimeOffset(unixTime * 10000000 + UnixEpoch.Ticks, TimeSpan.Zero);
+    }
+}
