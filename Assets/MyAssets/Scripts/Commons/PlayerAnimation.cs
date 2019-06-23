985d7d79c542886e7c0efc27812b4199987943ec
yonikkusu <yonikkusu@gmail.com>
2019-06-23 12:26:06 +0900

    add: キャラクター移動

---
+++ Assets/MyAssets/Scripts/Commons/PlayerAnimation.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+//--------------------------------------------------------------------------/
+/// <summary>
+/// プレイヤーのアニメーション管理クラス
+/// </summary>
+//--------------------------------------------------------------------------/
+public class PlayerAnimation : MonoBehaviour
+{
+    /// <summary>アニメーション速度</summary>
+    private const float speed = 30f;
+
+    [SerializeField] private Rigidbody2D rigidBody;
+
+    private Animator anim;
+
+    //--------------------------------------------------------------------------/
+    /// <summary>
+    /// 起動時処理
+    /// </summary>
+    //--------------------------------------------------------------------------/
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    //--------------------------------------------------------------------------/
+    /// <summary>
+    /// アップデート処理
+    /// </summary>
+    //--------------------------------------------------------------------------/
+    void Update()
+    {
+        var x = Input.GetAxisRaw("Horizontal");
+        var y = Input.GetAxisRaw("Vertical");
+
+        var direction = new Vector2(x, y).normalized;
+        rigidBody.velocity = direction * speed;
+
+        // アニメーショントリガー
+        if(Input.GetKey(KeyCode.LeftArrow)) { 
+            anim.SetTrigger("left");
+        }
+        else if(Input.GetKey(KeyCode.RightArrow)) {
+            anim.SetTrigger("right");
+        }
+        else if(Input.GetKey(KeyCode.UpArrow)) {
+            anim.SetTrigger("up");
+        }
+        else if(Input.GetKey(KeyCode.DownArrow)) {
+            anim.SetTrigger("down");
+        }
+    }
+}
---
+++ Assets/MyAssets/Scripts/Commons/PlayerCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    private GameObject player;
+
+    //--------------------------------------------------------------------------/
+    /// <summary>
+    /// 起動時処理
+    /// </summary>
+    //--------------------------------------------------------------------------/
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    //--------------------------------------------------------------------------/
+    /// <summary>
+    /// アップデート処理
+    /// </summary>
+    //--------------------------------------------------------------------------/
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+    }
+}
