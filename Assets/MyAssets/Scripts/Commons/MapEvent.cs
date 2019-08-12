d76b0c3039c1455785e64041fbdbf8f2dea8263b
yonikkusu <yonikkusu@gmail.com>
2019-08-13 00:21:26 +0900

    add: マップシーン基底クラス、マップイベント(調べる機能)

---
+++ Assets/MyAssets/Scripts/Commons/MapEvent.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+//--------------------------------------------------------------------------/
+/// <summary>
+/// マップイベント
+/// </summary>
+//--------------------------------------------------------------------------/
+public class MapEvent : MonoBehaviour
+{
+    [SerializeField] private Vector2 eventCoord = default;
+
+    //--------------------------------------------------------------------------/
+    /// <summary>
+    /// 調べるイベントを発動させるかチェックする
+    /// </summary>
+    /// <param name="checkedPosition">調べた位置</param>
+    //--------------------------------------------------------------------------/
+    public void CheckInspectEvent(Vector2 checkedPosition)
+    {
+        if(isSamePosition(checkedPosition)) {
+            onInspected();
+        }
+    }
+
+    //--------------------------------------------------------------------------/
+    /// <summary>
+    /// 調べられた時の処理(継承先で中身を定義する)
+    /// </summary>
+    //--------------------------------------------------------------------------/
+    protected virtual void onInspected() { }
+
+    //--------------------------------------------------------------------------/
+    /// <summary>
+    /// 指定した座標に対応するイベントがあるか
+    /// </summary>
+    /// <param name="position">座標</param>
+    /// <returns>あればtrue</returns>
+    //--------------------------------------------------------------------------/
+    private bool isSamePosition(Vector2 position)
+    {
+        if(eventCoord.x - 0.5 <= position.x && position.x <= eventCoord.x + 0.5 && 
+           eventCoord.y - 0.5 <= position.y && position.y <= eventCoord.y + 0.5) {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ Assets/MyAssets/Scripts/Commons/MapSceneBase.cs
@@ -0,0 +1,29 @@
+﻿//#define DEBUG_LOG
+
+using UnityEngine;
+
+//--------------------------------------------------------------------------/
+/// <summary>
+/// マップシーン基底クラス
+/// </summary>
+//--------------------------------------------------------------------------/
+public class MapSceneBase : MonoBehaviour
+{
+    [SerializeField] private MapEvent[] mapEvents = default;
+
+    //--------------------------------------------------------------------------/
+    /// <summary>
+    /// 調べるイベントを発動させるかチェックする
+    /// </summary>
+    /// <param name="checkedPosition">調べた位置</param>
+    //--------------------------------------------------------------------------/
+    public void CheckInspectEvents(Vector2 checkedPosition)
+    {
+#if DEBUG_LOG
+        Debug.Log($"調べた位置({checkedPosition.x}, {checkedPosition.y})");
+#endif
+        foreach(var mapEvent in mapEvents) {
+            mapEvent.CheckInspectEvent(checkedPosition);
+        }
+    }
+}
