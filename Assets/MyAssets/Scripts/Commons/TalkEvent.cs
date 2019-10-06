0a65e9c3536eddcc13f7c77727f322564965f825
yonikkusu <yonikkusu@gmail.com>
2019-10-06 21:58:22 +0900

    add: 会話イベントクラス追加

---
+++ Assets/MyAssets/Scripts/Commons/TalkEvent.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Fungus;
+
+//--------------------------------------------------------------------------/
+/// <summary>
+/// 会話イベント
+/// InspectorのmessageにFlowchartで発動させたい会話のメッセージIDを設定する
+/// </summary>
+//--------------------------------------------------------------------------/
+public class TalkEvent : MapEvent
+{
+    [SerializeField] private string message = default;
+    [SerializeField] private Flowchart flowchart = default;
+
+    //--------------------------------------------------------------------------/
+    /// <summary>
+    /// 調べられた時の処理
+    /// </summary>
+    //--------------------------------------------------------------------------/
+    protected override void onInspected()
+    {
+        flowchart.SendFungusMessage(message);
+    }
+}
