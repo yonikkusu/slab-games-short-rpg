9ef7fa36a18822a09c5d80fe4c03e87a829df19c
yonikkusu <yonikkusu@gmail.com>
2019-11-09 22:26:19 +0900

    add: アイテムデータ作成

---
+++ Assets/MyAssets/Scripts/Commons/Item/ItemDataList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+//--------------------------------------------------------------------------/
+/// <summary>
+/// アイテムデータリスト
+/// </summary>
+//--------------------------------------------------------------------------/
+[CreateAssetMenu]
+public class ItemDataList : ScriptableObject
+{
+    [SerializeField] private List<ItemData> itemList = new List<ItemData>();
+
+    /// <summary>アイテム一覧</summary>
+    public List<ItemData> ItemList => itemList;
+}
+
+//--------------------------------------------------------------------------/
+/// <summary>
+/// アイテムデータ
+/// </summary>
+//--------------------------------------------------------------------------/
+[Serializable]
+public class ItemData
+{
+    [SerializeField] private string name = default;
+    [SerializeField] private Sprite sprite = default;
+
+    /// <summary>アイテム名</summary>
+    public string Name => name;
+    /// <summary>アイテム画像</summary>
+    public Sprite Sprite => sprite;
+}
+
+//--------------------------------------------------------------------------/
+/// <summary>
+/// アイテムID
+/// </summary>
+//--------------------------------------------------------------------------/
+public enum ItemID
+{
+    Key,
+    Book,
+}
