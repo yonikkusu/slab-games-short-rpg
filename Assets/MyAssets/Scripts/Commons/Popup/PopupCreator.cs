4d717948c92b08ea3b17a85d0b272c725a87b859
yonikkusu <yonikkusu@gmail.com>
2021-08-22 12:05:56 +0900

    add: システムポップアップ作成

---
+++ Assets/MyAssets/Scripts/Commons/Popup/PopupCreator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// ポップアップ生成クラス
+/// </summary>
+public class PopupCreator : SingletonMonoBehaviour<PopupCreator>
+{
+    [SerializeField] private SystemPopup systemPopup;
+
+    /// <summary>
+    /// ポップアップを生成する
+    /// </summary>
+    /// <returns>ポップアップ</returns>
+    public SystemPopup CreatePopup() => systemPopup;
+}
---
+++ Assets/MyAssets/Scripts/Commons/Popup/SystemPopup.cs
@@ -0,0 +1,85 @@
+﻿using System.Threading;
+using UniRx;
+using UniRx.Async;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// システムポップアップ
+/// </summary>
+public class SystemPopup : MonoBehaviour
+{
+    [SerializeField] private GameObject popupObject;
+    [SerializeField] private Button closeBackgroundButton;
+    [SerializeField] private Text messageText;
+
+    private UniTaskCompletionSource source;
+    private CancellationTokenSource cancellationTokenSource;
+
+    /// <summary>
+    /// 起動時処理
+    /// </summary>
+    void Start()
+    {
+        closeBackgroundButton.OnClickAsObservable()
+            .Subscribe(_ => source?.TrySetResult())
+            .AddTo(this);
+    }
+
+    /// <summary>
+    /// ポップアップを表示する
+    /// </summary>
+    /// <param name="message">表示するメッセージ</param>
+    /// <returns>UniTask</returns>
+    public async UniTask ShowAsync(string message)
+    {
+        initialize(message);
+        show();
+        cancellationTokenSource = new CancellationTokenSource();
+        waitKeyInputAsync(cancellationTokenSource.Token).Forget();
+        await waitHideAsync();
+    }
+
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    /// <param name="message">表示するメッセージ</param>
+    private void initialize(string message) => messageText.text = message;
+
+    /// <summary>
+    /// ポップアップを表示する
+    /// </summary>
+    private void show() => popupObject.gameObject.SetActive(true);
+
+    /// <summary>
+    /// ポップアップを非表示にする
+    /// </summary>
+    private void hide() => popupObject.gameObject.SetActive(false);
+
+    /// <summary>
+    /// キー入力を待つ
+    /// </summary>
+    /// <param name="cancellationToken">キャンセル通知</param>
+    /// <returns>UniTaskVoid</returns>
+    private async UniTaskVoid waitKeyInputAsync(CancellationToken cancellationToken)
+    {
+        // NOTE: 1秒間は入力をブロック
+        await UniTask.Delay(1000, cancellationToken: cancellationToken);
+        await UniTask.WaitUntil(() => Input.GetKey(KeyCode.Return), cancellationToken: cancellationToken);
+        source?.TrySetResult();
+    }
+
+    /// <summary>
+    /// 閉じられるのを待つ
+    /// </summary>
+    /// <returns>UniTask</returns>
+    private async UniTask waitHideAsync()
+    {
+        source = new UniTaskCompletionSource();
+        await source.Task;
+        cancellationTokenSource.Cancel();
+        cancellationTokenSource.Dispose();
+        source = null;
+        hide();
+    }
+}
