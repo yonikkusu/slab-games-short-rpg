248baa16b2bb9d413f515d44c2833a732cb34bb7
yonikkusu <yonikkusu@gmail.com>
2021-08-29 14:42:38 +0900

    add: 入力モード変更クラスと入力処理ベースクラスを作成

---
+++ Assets/MyAssets/Scripts/Commons/Input/InputBase.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 入力処理 ベースクラス
+/// </summary>
+public abstract class InputBase : MonoBehaviour
+{
+    /// <summary>使用する入力モード</summary>
+    protected abstract InputMode usedMode { get; }
+
+    /// <summary>
+    /// 更新する
+    /// </summary>
+    void Update()
+    {
+        if(InputService.Instance.CurrentMode != usedMode) return;
+        checkInput();
+    }
+
+    /// <summary>
+    /// 入力をチェックする
+    /// </summary>
+    protected abstract void checkInput();
+}
---
+++ Assets/MyAssets/Scripts/Commons/Input/InputService.cs
@@ -0,0 +1,24 @@
+﻿/// <summary>
+/// 入力 Serviceクラス
+/// </summary>
+public class InputService : SingletonMonoBehaviour<InputService>
+{
+    /// <summary>現在のモード</summary>
+    public InputMode CurrentMode { get; private set; }
+
+    /// <summary>
+    /// 入力モードを変更する
+    /// </summary>
+    /// <param name="mode">入力モード</param>
+    public void ChangeMode(InputMode mode) => CurrentMode = mode;
+}
+
+/// <summary>
+/// 入力モード
+/// </summary>
+public enum InputMode
+{
+    None = 0,
+    Normal,
+    SystemPopup,
+}
