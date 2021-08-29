dee1a0b8d7bda87e956ed9f9a04e3db609e1a01d
yonikkusu <yonikkusu@gmail.com>
2021-08-29 14:45:05 +0900

    clean: システムポップアップの入力処理をInputBaseに置き換え

---
+++ Assets/MyAssets/Scripts/Commons/Popup/SystemPopupInput.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UniRx;
+
+/// <summary>
+/// システムポップアップ 入力クラス
+/// </summary>
+public class SystemPopupInput : InputBase
+{
+    /// <summary>使用する入力モード</summary>
+    protected override InputMode usedMode => InputMode.SystemPopup;
+
+    /// <summary>閉じるキー入力通知</summary>
+    public IObservable<Unit> OnCloseKeyDown => onCloseKeyDownSubject;
+    private Subject<Unit> onCloseKeyDownSubject = new Subject<Unit>();
+
+    /// <summary>
+    /// 入力をチェックする
+    /// </summary>
+    protected override void checkInput()
+    {
+        if(Input.GetKeyDown(KeyCode.Return)) {
+            onCloseKeyDownSubject.OnNext(Unit.Default);
+        }
+    }
+}
