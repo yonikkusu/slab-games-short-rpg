3b5e11db8267997778ba0d939737a1d1f28c1824
segaWorld <biton1993@gmail.com>
2021-09-11 20:45:51 +0900

    add: パスワード入力画面のスクリプト

---
+++ Assets/MyAssets/Scripts/Commons/Event/InputPassword.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx.Async;
+
+public class InputPassword : MapEvent
+{
+    
+    /// <summary>
+    /// 調べられた時の処理
+    /// </summary>
+    /// <returns>UniTask</returns>
+    protected override async UniTask onInspectedAsync()
+    {
+        //Debug.Log("input password");
+        PasswordPresenter.Instance.Initialize();
+        var password = PasswordPresenter.Instance.getPassword();
+        //PasswordPresenter.Instance.hide();
+
+        Debug.Log(password.ToString());
+    }
+
+}
---
+++ Assets/MyAssets/Scripts/Commons/Password/PasswordPresenter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx.Async;
+
+public class PasswordPresenter : SingletonMonoBehaviour<PasswordPresenter>
+{
+    [SerializeField] private Image PasswordPanel;
+    [SerializeField] private PasswordCtrlText passwordCtrlText;
+
+    public void Initialize()
+    {
+        Debug.Log("presenter initialize");
+        PasswordPanel.gameObject.SetActive(true);
+        passwordCtrlText.Initialize();
+    }
+
+    public int getPassword()
+    {
+        while( !Input.GetKey(KeyCode.Return) )
+        {
+            ;
+        }
+
+        Debug.Log("get password");
+        return passwordCtrlText.getPassword();
+    }
+
+    public void hide()
+    {
+        Debug.Log("hide presenter");
+        PasswordPanel.gameObject.SetActive(false);
+    }
+  
+}
