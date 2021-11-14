a31dc63d0bfdd5973c39b94ebdc43feb86b7d364
yonikkusu <yonikkusu@gmail.com>
2021-11-14 14:33:17 +0900

    add: 各部屋の扉を開けるイベントID追加

--- Assets/MyAssets/Scripts/Commons/Event/UseItemEvent/OpenEventId.cs
+++ Assets/MyAssets/Scripts/Commons/Event/UseItemEvent/OpenEventId.cs
@@ -3,7 +3,16 @@
 /// </summary>
 public enum OpenEventId
 {
-    None,
-    ExtinguishFire,
-    OpenKitchen,
+    None = 0,
+    ExtinguishFire = 1,
+    OpenKitchen = 2,
+    OpenBathRoom = 3,
+    OpenDressingRoom = 4,
+    OpenGarden = 5,
+    OpenLibrary = 6,
+    OpenSekkanRoom = 7,
+    OpenToilet = 8,
+    OpenStudy = 9,
+    OpenBedRoom = 10,
+    OpenKidsRoom = 11,
 }
