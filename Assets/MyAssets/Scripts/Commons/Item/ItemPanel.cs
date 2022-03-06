1ef9cfbcb1ad6fde2f28ad8184958e1db77a524b
yonikkusu <yonikkusu@gmail.com>
2022-03-06 12:05:14 +0900

    update: アイテムパネルのノードを動的生成できるようにした

--- Assets/MyAssets/Scripts/Commons/Item/ItemPanel.cs
+++ Assets/MyAssets/Scripts/Commons/Item/ItemPanel.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 using UniRx;
 
@@ -7,17 +8,21 @@ using UniRx;
 /// </summary>
 public class ItemPanel : SingletonMonoBehaviour<ItemPanel>
 {
-    [SerializeField] private Image[] itemImageList = default;
+    [SerializeField] private ItemPanelNode itemPanelNodeBase = default;
+    [SerializeField] private Transform nodeParent = default;
     [SerializeField] private Image selectFrame = default;
     [SerializeField] private ItemSynthesisPresenter synthesisPresenter = default;
     [SerializeField] private Button synthesisButton = default;
     [SerializeField] private ItemPanelInput itemPanelInput = default;
+
+    private List<ItemPanelNode> nodeList;
 
     /// <summary>
     /// 起動時処理
     /// </summary>
     void Start()
     {
+        itemPanelNodeBase.gameObject.SetActive(false);
         synthesisPresenter.Initialize();
         synthesisButton.OnClickAsObservable().Subscribe(_ => synthesisPresenter.Show()).AddTo(this);
         itemPanelInput.OnWheelValueChanged.Subscribe(moveCursor).AddTo(this);
@@ -28,7 +33,8 @@ public class ItemPanel : SingletonMonoBehaviour<ItemPanel>
     /// </summary>
     public void Initialize()
     {
-        RenderView();
+        createNodes();
+        moveSelectFrame(nodeIndex: 0);
     }
 
     /// <summary>
@@ -36,14 +42,32 @@ public class ItemPanel : SingletonMonoBehaviour<ItemPanel>
     /// </summary>
     public void RenderView()
     {
-        foreach(var itemImage in itemImageList) {
-            itemImage.sprite = null;
-        }
+        nodeList?.ForEach(node => node.Initialize(ItemID.None));
         var possessionItemList = PlayerData.Instance.ItemManager?.PossessionItemList;
         if(possessionItemList == null) return;
 
-        for(var i = 0; i < possessionItemList.Count; i++) {
-            itemImageList[i].sprite = possessionItemList[i].Sprite;
+        for(var i = 0; i < nodeList.Count; i++) {
+            if((possessionItemList.Count - 1) < i) {
+                nodeList[i].Initialize(ItemID.None);
+                continue;
+            }
+            var itemId = possessionItemList[i].ID;
+            nodeList[i].Initialize(itemId);
+        }
+    }
+
+    /// <summary>
+    /// ノード生成
+    /// </summary>
+    private void createNodes()
+    {
+        nodeList?.ForEach(node => Destroy(node.gameObject));
+        nodeList = new List<ItemPanelNode>();
+        for(var i = 0; i < ItemManager.MaxItemNum; i++) {
+            var node = Instantiate(itemPanelNodeBase, parent: nodeParent);
+            node.Initialize(ItemID.None);
+            node.gameObject.SetActive(true);
+            nodeList.Add(node);
         }
     }
 
@@ -59,7 +83,19 @@ public class ItemPanel : SingletonMonoBehaviour<ItemPanel>
         var index = wheelValue > 0f ? selectedItemIndex.Value - 1 : selectedItemIndex.Value + 1;
         PlayerData.Instance.ItemManager.UpdateSelectedItem(index);
         var newIndex = PlayerData.Instance.ItemManager.SelectedItemIndex;
-        selectFrame.transform.SetParent(itemImageList[newIndex].transform);
+        moveSelectFrame(newIndex);
+    }
+
+    /// <summary>
+    /// 選択フレームを移動する
+    /// </summary>
+    /// <param name="nodeIndex">ノードIndex</param>
+    private void moveSelectFrame(int nodeIndex)
+    {
+        var hasTargetNode = (nodeList.Count - 1) < nodeIndex;
+        selectFrame.gameObject.SetActive(hasTargetNode);
+        if(!hasTargetNode) return;
+        selectFrame.transform.SetParent(nodeList[nodeIndex].transform);
         selectFrame.transform.localPosition = new Vector3();
     }
 }
---
+++ Assets/MyAssets/Scripts/Commons/Item/ItemPanelNode.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// アイテムパネル Node
+/// </summary>
+public class ItemPanelNode : MonoBehaviour
+{
+    [SerializeField] private Image itemImage;
+
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    /// <param name="itemId">アイテムID</param>
+    public void Initialize(ItemID itemId)
+    {
+        var itemMaster = MasterGetter.GetItemData(itemId);
+        itemImage.sprite = itemMaster?.Sprite;
+    }
+}
